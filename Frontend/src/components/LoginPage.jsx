412086738ee5cb184ac72160e51e71428854d979
AkhilaJaya123 <akhilaj2003@gmail.com>
2025-04-16 19:25:11 +0530

    Update LoginPage.jsx

--- Frontend/src/components/LoginPage.jsx
+++ Frontend/src/components/LoginPage.jsx
@@ -7,8 +7,6 @@ function LoginPage() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const apiUrl = import.meta.env.VITE_API_URL;
-
   const handleLogin = async () => {
     if (!user.username || !user.password || !user.role) {
       setError("All fields are required");
@@ -16,8 +14,7 @@ function LoginPage() {
     }
 
     try {
-      const response = await axios.post(`${apiUrl}/api/login`, user);
-
+      const response = await axios.post("http://localhost:8000/api/login", user);
       if (response.data.role === "faculty") {
         navigate("/faculty");
       } else if (response.data.role === "student") {
